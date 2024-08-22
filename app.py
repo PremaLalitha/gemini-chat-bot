aeb4cd802aa6c4769c9f034c467e4aea8d1e5252
PremaLalitha <premalalitha08@gmail.com>
2024-08-22 17:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import google.generativeai as genai #pip install google-generativeai
 
 
-st.title("Welcome to PL chat")
+st.title("Welcome to Infobot")
 
 genai.configure(api_key="AIzaSyD0AJ42-kPJkv_dueFv8wS2xoXpOvnOtmU")
 
-text = st.text_input("enter your quetion")
+text = st.text_input("Enter Your Question")
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
